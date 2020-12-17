2b07915dd9daaa95553802d61018029fda5c4ac8
ttttrelluder <75843272+ttttrelluder@users.noreply.github.com>
2020-12-16 18:14:59 -0600

    Added a new script
    Don't even use this yet, it's not finished and no where near finished. Only changes your walk speed with a slider.

---
+++ meh wip.lua
@@ -0,0 +1,31 @@
+local Player = game.Players.LocalPlayer
+local Character = Player.Character
+local Humanoid = Character.Humanoid
+local Library = loadstring(game:HttpGet(('https://raw.githubusercontent.com/bloodball/-back-ups-for-libs/main/wall%20v3')))()
+local NiggaWTab = Library:CreateWindow("NiggaWare")
+local NiggaMTab = NiggaWTab:CreateFolder("Main")
+
+NiggaMTab:Button("Button",function()
+    print("Elym Winning")
+end)
+
+NiggaMTab:Toggle("Toggle",function(bool)
+    Toggle.Toggle = bool
+    print(Toggle.Toggle)
+end)
+
+NiggaMTab:Slider("Walk Speed",{
+    min = 10;
+    max = 50;
+    precise = false;
+},function(WSVal)
+    Humanoid.WalkSpeed = WSVal
+end)
+
+NiggaMTab:Dropdown("Dropdown",{"A","B","C"},true,function(mob)
+    print(mob)
+end)
+
+NiggaMTab:Bind("Refresh",Enum.KeyCode.C,function()
+    Character:Destroy()
+end)
